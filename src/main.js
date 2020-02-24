b84010512a4d4b9286361ea1a3b930319af8d468
shantanu.patra <shantanu.patra@webspiders.com>
2020-02-24 13:59:47 +0530

    Test commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+//import Vue from 'vue'
+import Vue from 'vue/dist/vue.js';
+//import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
